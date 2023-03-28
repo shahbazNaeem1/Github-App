0632f53cf80a706f9dea14aec5b21352fda77b13
Shahbaz Naeem <mail.shahbaznaeem1@gmail.com>
2023-03-28 14:52:35 +0500

    Android build configurations

--- android/app/src/main/java/com/githubapp/MainActivity.java
+++ android/app/src/main/java/com/githubapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.github_app;
+package com.githubapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "github_app";
+    return "githubapp";
   }
 }
